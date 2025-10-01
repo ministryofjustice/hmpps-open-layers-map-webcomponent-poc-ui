8161d620d9574ecf4132b6014260afe6ca5af7ee
Ben Carter <ben.carter@justice.gov.uk>
2025-10-01 15:34:10 +0100

    Add styles

---
+++ src/scripts/map/styles/arrow.ts
@@ -0,0 +1,20 @@
+import { Coordinate } from "ol/coordinate";
+import { Point } from "ol/geom";
+import { Fill, RegularShape, Style } from "ol/style";
+
+class ArrowStyle extends Style {
+  constructor(coordinate: Coordinate, radius: number, rotation: number) {
+    super({
+      geometry: new Point(coordinate),
+      image: new RegularShape({
+        points: 3,
+        radius,
+        fill: new Fill({ color: 'black' }),
+        rotation,
+        rotateWithView: true,
+      }),
+    })
+  }
+}
+
+export default ArrowStyle
---
+++ src/scripts/map/styles/line.ts
@@ -0,0 +1,14 @@
+import { Stroke, Style } from "ol/style"
+
+class LineStyle extends Style {
+  constructor(width: number) {
+    super({
+      stroke: new Stroke({
+        width,
+        color: 'black',
+      }),
+    })
+  }
+}
+
+export default LineStyle
