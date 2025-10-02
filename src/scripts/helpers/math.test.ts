019959c3f5c39c2a1f7b07e9b2ad5a1e637466ae
Ben Carter <ben.carter@justice.gov.uk>
2025-10-02 11:49:16 +0100

    Add tests for clamp function

---
+++ src/scripts/helpers/math.test.ts
@@ -0,0 +1,16 @@
+import { clamp } from './math'
+
+describe('math', () => {
+  describe('clamp', () => {
+    it.each([
+      ['min', 0, 1, 5, 1],
+      ['max', 6, 1, 5, 5],
+      ['mid', 3, 1, 5, 3],
+    ])(
+      '[%s] clamp(%s, %s, %s)',
+      (_: string, value: number, min: number, max: number, expected: number) => {
+        expect(clamp(value, min, max)).toEqual(expected)
+      },
+    )
+  })
+})
