e3088bd38856e855262ec7e81a6a40cd368f7f39
Ben Carter <ben.carter@justice.gov.uk>
2025-10-03 12:23:41 +0100

    Remove export

--- src/scripts/map/layers/index.ts
+++ src/scripts/map/layers/index.ts
@@ -4,7 +4,6 @@ export { LinesLayer } from './lines-layer'
 export { ArrowsLayer } from './arrows-layer'
 export { TracksLayer } from './tracks-layer'
 export { NumberingLayer } from './numbering-layer'
-export { TrackLinesLayer } from './track-lines-layer'
 
 export type { LocationsLayerOptions } from './locations-layer'
 export type { CirclesLayerOptions } from './circles-layer'
