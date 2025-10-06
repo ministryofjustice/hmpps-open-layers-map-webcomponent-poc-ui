20315c2dbb1345abc0c2a4bc3d6f56e98c2b87ea
Patrick O'Donnell <patrick.odonnell@justice.gov.uk>
2025-10-06 11:11:28 +0100

    Set CSS to handle default internal styles and overrides using part. Update sandbox template to use class not :part

--- src/scripts/map/overlays/feature-overlay.ts
+++ src/scripts/map/overlays/feature-overlay.ts
@@ -44,22 +44,10 @@ export default class FeatureOverlay extends Overlay {
   }
 
   private replacer(html: string, data: Record<string, unknown>) {
-    const populated = html.replace(/{{(.*?)}}/g, (_, key) => {
+    return html.replace(/{{(.*?)}}/g, (_, key) => {
       const value = data[key.trim()]
       return value !== undefined ? String(value) : ''
     })
-
-    // Add zebra-striping parts to overlay rows
-    const frag = document.createElement('div')
-    frag.innerHTML = populated
-
-    Array.from(frag.querySelectorAll('[part~="app-map__overlay-row"]')).forEach((row, index) => {
-      const isEven = (index + 1) % 2 === 0
-      const partValue = row.getAttribute('part') || ''
-      row.setAttribute('part', `${partValue} ${isEven ? 'app-map__overlay-row--even' : 'app-map__overlay-row--odd'}`)
-    })
-
-    return frag.innerHTML
   }
 
   showAtCoordinate(coordinate: Coordinate, data: Record<string, unknown>) {
